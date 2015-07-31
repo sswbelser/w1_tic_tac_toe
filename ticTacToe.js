d4861477c402e1e18b759c9526786d0f79b47312
Sabastian <sswbelser@gmail.com>
2015-07-30 22:18:23 -0700

    jQuery fix

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,156 +1,109 @@
-window.xTurn = true;
-window.addEventListener("DOMContentLoaded", function() {
-	var sq1 = document.getElementById("top-left");
-  	var sq2 = document.getElementById("top-middle");
-  	var sq3 = document.getElementById("top-right");
-  	var sq4 = document.getElementById("middle-left");
-  	var sq5 = document.getElementById("middle-middle");
-  	var sq6 = document.getElementById("middle-right");
-  	var sq7 = document.getElementById("bottom-left");
-  	var sq8 = document.getElementById("bottom-middle");
-  	var sq9 = document.getElementById("bottom-right");
+// wait for the DOM to finish loading
+$(function() {
 
-  	var reset = function() {
-  		window.xTurn = true
-  		sq1.innerText = ""
-  		sq2.innerText = ""
-  		sq3.innerText = ""
-  		sq4.innerText = ""
-  		sq5.innerText = ""
-  		sq6.innerText = ""
-  		sq7.innerText = ""
-  		sq8.innerText = ""
-  		sq9.innerText = ""
-  	}
+  // select all the necessary elements
+  var $board = $('#board'),
+      $boxes = $('.box'),
+      $reset = $('#reset');
+  
+  // player X always goes first
+  var turn = "X";
 
-  	var changeText = function() {
-    	sq1.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq2.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq3.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq4.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq5.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq6.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq7.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq8.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();
-    	})
-    	sq9.addEventListener("click", function(event){
-      		addPosition(this);
-      		checkWinner();  
-    	})
-  	}
-  	checkWinner();
-  	changeText();
+  // keep track of moves count
+  var moves = 0;
+  
+  // helper function to change turn based on current turn
+  var changeTurn = function() {
+    if (turn === "X") {
+      turn = "O";
+    } else {
+      turn = "X";
+    }
+  };
 
-	function addPosition (element) {
-  		if (element.innerText == ""){
-    	if (window.xTurn == true) {
-      		element.innerText = "X";
-    	}
-    	else {
-      		element.innerText = "O";
-    	}
-    	window.xTurn = !window.xTurn;
-  		}
-	}
+  // helper function to reset board
+  var resetBoard = function() {
+    $boxes.text("");
+    $boxes.removeClass("X").removeClass("O");
 
-	function checkWinner() {
-    	if (sq1.innerText == "X" && sq2.innerText == "X" && sq3.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq4.innerText == "X" && sq5.innerText == "X" && sq6.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      	reset();
-    	}
-    	else if (sq7.innerText == "X" && sq8.innerText == "X" && sq9.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq1.innerText == "X" && sq4.innerText == "X" && sq7.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq2.innerText == "X" && sq5.innerText == "X" && sq8.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq3.innerText == "X" && sq6.innerText == "X" && sq9.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq1.innerText == "X" && sq5.innerText == "X" && sq9.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq3.innerText == "X" && sq5.innerText == "X" && sq7.innerText == "X") {
-      		alert("Player 'X' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq1.innerText == "O" && sq2.innerText == "O" && sq3.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq4.innerText == "O" && sq5.innerText == "O" && sq6.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq7.innerText == "O" && sq8.innerText == "O" && sq9.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq1.innerText == "O" && sq4.innerText == "O" && sq7.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq2.innerText == "O" && sq5.innerText == "O" && sq8.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq3.innerText == "O" && sq6.innerText == "O" && sq9.innerText== "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq1.innerText == "O" && sq5.innerText == "O" && sq9.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else if (sq3.innerText == "O" && sq5.innerText == "O" && sq7.innerText == "O") {
-      		alert("Player 'O' wins! Press OK to reset game");
-      		reset();
-    	}
-    	else {
-    		checkStalemate();
-    	}
-  	}
+    // player X always goes first
+    turn = "X";
+    
+    // reset moves count
+    moves = 0;
+  };
 
-	function checkStalemate() {
-  		if (sq1.innerText != "" && sq2.innerText != "" && sq3.innerText != "" && sq4.innerText != "" && sq5.innerText != "" && sq6.innerText != "" && sq7.innerText != "" && sq8.innerText != "" && sq9.innerText != "") {
-  			alert("Stalemate, press OK to try again");
-  			reset();
-  		}
- 	}
+  // helper function to check for wins in three boxes
+  var allThree = function(player, box1, box2, box3) {
+    return (box1.innerText === player) && (box2.innerText === player) && (box3.innerText === player);
+  };
+
+  // check for wins across both diagonals
+  var winsDiagonal = function(player) {
+    return allThree(player, $boxes[0], $boxes[4], $boxes[8]) ||
+           allThree(player, $boxes[2], $boxes[4], $boxes[6]);
+  };
+
+  // check for wins down all columns
+  var winsColumn = function(player) {
+    return allThree(player, $boxes[0], $boxes[3], $boxes[6]) ||
+           allThree(player, $boxes[1], $boxes[4], $boxes[7]) ||
+           allThree(player, $boxes[2], $boxes[5], $boxes[8]);
+  };
+
+  // check for wins across all rows
+  var winsRow = function(player) {
+    return allThree(player, $boxes[0], $boxes[1], $boxes[2]) ||
+           allThree(player, $boxes[3], $boxes[4], $boxes[5]) ||
+           allThree(player, $boxes[6], $boxes[7], $boxes[8]);
+  };
+
+  // player is winner if wins row, column, or diagonal
+  var winnerIs = function(player) {
+    return winsRow(player) || winsColumn(player) || winsDiagonal(player);
+  };
+
+  // helper function to check for winner
+  var getWinner = function() {
+    if (winnerIs("X")) {
+      return "X";
+    }
+    else if (winnerIs("O")) {
+      return "O";
+    }
+    else {
+      return null;
+    }
+  };
+
+  // listen for clicks on boxes to play the game
+  $boxes.on('click', function() {
+    // only allow move if box is blank
+    if ($(this).text() === "") {
+      $(this).text(turn);
+      $(this).addClass(turn);
+      moves += 1;
+      
+      // check for a winner if 5 or more moves have been played
+      if (moves >= 5) {
+        var winner = getWinner();
+    
+        // if there is a winner, alert the winner and reset the game
+        if (winner) {
+          alert("Player " + winner + " won!");
+          resetBoard();
+        } else {
+          changeTurn();
+        }
+      } else {
+        changeTurn();
+      }
+    }
+  });
+
+  // listen for clicks on `reset` button to reset the board
+  $reset.on('click', function () {
+    resetBoard();
+  });
 
 });
